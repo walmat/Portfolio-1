abea3a7af3c8ebf5a638c43b257eec15991ac2d9
Matthew Wall <matthew.wallt@gmail.com>
2016-09-19 19:48:54 -0500

    Login.java
    updated login GUI for portfolio 1

--- cs319-lab01/src/lab01/Login.java
+++ cs319-lab01/src/lab01/Login.java
@@ -1,33 +1,41 @@
-package lab01;
+package portfolio1;
 
-import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-
 import java.awt.event.KeyAdapter;
-import java.awt.Component;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.swing.Box;
-import javax.swing.InputVerifier;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 public class Login extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField nameField;
-	private String name;
-	private boolean giveAdminProperties = false;
+	private String color;
+	private String email;
+	private String password;
+	private String username;
+	private boolean giveHostProperties = false;
+	private JTextField emailField;
+	private JTextField passwordField;
+	private JTextField usernameField;
+	
+	public static final Pattern VALID_EMAIL_ADDRESS_REGEX = 
+		    Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+	
+	
 
 	/**
 	 * Launch the application.
@@ -49,89 +57,134 @@ public class Login extends JFrame {
 	 * Create the frame.
 	 */
 	public Login() {
-		setTitle("Login Page");
+		setTitle("Create Account");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 279, 271);
+		setBounds(100, 100, 279, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		String[] options = new String[] {"Green", "Blue",
+                "Red", "Orange"};
+		JComboBox comboBox = new JComboBox(options);
+		comboBox.setBounds(80, 169, 135, 27);
+		contentPane.add(comboBox);
 
-		JLabel lblNewLabel = new JLabel("Connect");
+		JLabel lblNewLabel = new JLabel("Create Your Profile");
 		lblNewLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 26));
-		lblNewLabel.setBounds(87, 21, 107, 32);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblNewLabel.setBounds(44, 21, 195, 32);
 		contentPane.add(lblNewLabel);
 
-		JLabel lblEnterYourName = new JLabel("Enter Your Name");
-		lblEnterYourName.setAlignmentX(Component.CENTER_ALIGNMENT);
-		lblEnterYourName.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblEnterYourName.setBounds(10, 65, 117, 23);
-		contentPane.add(lblEnterYourName);
+		JLabel lblUsername = new JLabel("Username");
+		lblUsername.setAlignmentX(Component.CENTER_ALIGNMENT);
+		lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblUsername.setBounds(10, 65, 72, 23);
+		contentPane.add(lblUsername);
 
-		nameField = new JTextField();
+		usernameField = new JTextField();
+		usernameField.setBounds(112, 68, 161, 20);
+		contentPane.add(usernameField);
+		usernameField.setColumns(10);
 
-		nameField.addKeyListener(new KeyAdapter() {
+		JButton loginButton = new JButton("Create");
+		loginButton.setBounds(96, 220, 89, 23);
+		contentPane.add(loginButton);
+
+		Component horizontalStrut = Box.createHorizontalStrut(20);
+		horizontalStrut.setBounds(10, 32, 25, 12);
+		contentPane.add(horizontalStrut);
+
+		Component horizontalStrut_1 = Box.createHorizontalStrut(20);
+		horizontalStrut_1.setBounds(248, 32, 25, 12);
+		contentPane.add(horizontalStrut_1);
+		
+		JLabel lblEmail = new JLabel("Email");
+		lblEmail.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblEmail.setAlignmentX(0.5f);
+		lblEmail.setBounds(10, 134, 48, 23);
+		contentPane.add(lblEmail);
+		
+		emailField = new JTextField();
+		emailField.setColumns(10);
+		emailField.setBounds(70, 136, 203, 20);
+		contentPane.add(emailField);
+		
+		emailField.addKeyListener(new KeyAdapter() {
+
 			@Override
 			public void keyPressed(KeyEvent e) {
 
 				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-					// check for empty string here
-					name = nameField.getText().trim();
-					if (name.equals("")){
-						System.out.println("Please enter a valid name.");
+					// check for empty strings here
+					username = usernameField.getText().trim();
+					password = passwordField.getText().trim();
+					email = emailField.getText().trim();
+					color = options[comboBox.getSelectedIndex()];
+					if (username.equals("") || password.equals("") || email.equals("")){
+						System.out.println("Please fill out all fields.");
 						System.exit(-1);
 						dispose();
 					}
-
-					if (name.equalsIgnoreCase("Admin")) {
-						giveAdminProperties = true;
+					if (!validate(email)){
+						System.out.println("Please enter a valid email.");
+						System.exit(-1);
+						dispose();
 					}
-
-					EventQueue.invokeLater(new Runnable() {
-						public void run() {
-							try {
-								Client client = new Client("localhost", name, 1222, giveAdminProperties);
-							} catch (Exception e) {
-								e.printStackTrace();
+					else {
+						EventQueue.invokeLater(new Runnable() {
+							public void run() {
+								try {
+									Client client = new Client("localhost", username, password, email, color, 1222, giveHostProperties);
+								} catch (Exception e) {
+									e.printStackTrace();
+								}
 							}
-						}
-					});
-
-					// dispose of this login page
-					dispose();
+						});
+	
+						// dispose of this login page
+						dispose();
+					}
 				}
 			}
 		});
-		nameField.setBounds(123, 68, 150, 20);
-		contentPane.add(nameField);
-		nameField.setColumns(10);
+		
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblPassword.setAlignmentX(0.5f);
+		lblPassword.setBounds(10, 100, 72, 23);
+		contentPane.add(lblPassword);
+		
+		passwordField = new JTextField();
+		passwordField.setColumns(10);
+		passwordField.setBounds(112, 103, 161, 20);
+		contentPane.add(passwordField);
 
-		JButton loginButton = new JButton("Login");
-		loginButton.setBounds(91, 111, 89, 23);
-		contentPane.add(loginButton);
-
-		Component horizontalStrut = Box.createHorizontalStrut(20);
-		horizontalStrut.setBounds(10, 32, 56, 0);
-		contentPane.add(horizontalStrut);
-
-		Component horizontalStrut_1 = Box.createHorizontalStrut(20);
-		horizontalStrut_1.setBounds(217, 32, 56, 12);
-		contentPane.add(horizontalStrut_1);
 		loginButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				name = nameField.getText().trim();
-
-				if (name.equalsIgnoreCase("Admin")) {
-					giveAdminProperties = true;
+				username = usernameField.getText().trim();
+				password = passwordField.getText().trim();
+				email = emailField.getText().trim();
+				color = options[comboBox.getSelectedIndex()];
+				if (username.equals("") || password.equals("") || email.equals("")){
+					System.out.println("Please fill out all fields.");
+					System.exit(-1);
+					dispose();
 				}
-
+				if (!validate(email)){
+					System.out.println("Please enter a valid email.");
+					System.exit(-1);
+					dispose();
+				}
+				
 				EventQueue.invokeLater(new Runnable() {
 					public void run() {
 						try {
-							Client client = new Client("localhost", name, 1222, giveAdminProperties);
+							Client client = new Client("localhost", username, password, email, color, 1222, giveHostProperties);
 						} catch (Exception e) {
 							e.printStackTrace();
 						}
@@ -143,5 +196,10 @@ public class Login extends JFrame {
 
 			}
 		});
+	}
+	
+	public static boolean validate(String emailStr) {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
+        return matcher.find();
 	}
 }
